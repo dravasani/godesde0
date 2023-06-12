61c7e59bfc1e289d0a54e600225f5e3b5828322d
Diego Ravasani <dravasani@gmail.com>
2023-06-12 15:07:21 -0300

    primera subida de mi lab GOO

--- variables/enteras.go
+++ variables/enteras.go
@@ -2,11 +2,11 @@ package variables
 
 import "fmt"
 
-func MuestroEnteros(){
+func MuestroEnteros() {
 	var intcomun int
 	intde32 := int32(10)
 	intde64 := int64(100)
-	fmt.Println("int comun es igual ", intcomun)
-	fmt.Println("int 32 es igual ", intde32)
-	fmt.Println("int 64 es igual ", intde64)
+	fmt.Println("int comun es igual a ", intcomun)
+	fmt.Println("int 32 es igual a ", intde32)
+	fmt.Println("int 64 es igual a ", intde64)
 }
